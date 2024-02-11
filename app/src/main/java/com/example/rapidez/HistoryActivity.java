c1d54b76f9ea311f86f5631f0f27ee8a60f3b6d6
kiruthik-coder <spectacularkiruthik@gmail.com>
2024-02-11 18:26:50 +0530

    Version0.6 Update 2.2

--- app/src/main/java/com/example/rapidez/HistoryActivity.java
+++ app/src/main/java/com/example/rapidez/HistoryActivity.java
@@ -24,7 +24,7 @@ public class HistoryActivity extends AppCompatActivity {
         ratingtxt=findViewById(R.id.rating1txt);
         DatabaseReference reference= FirebaseDatabase.getInstance().getReference().child("newRequest");
         firebaseDatabase = FirebaseDatabase.getInstance();
-        ratingref=firebaseDatabase.getReference("S");
+        ratingref=firebaseDatabase.getReference("Score");
         getRating();
     }
 
@@ -38,7 +38,7 @@ public class HistoryActivity extends AppCompatActivity {
                 // changed in our Firebase console.
                 // below line is for getting the data from
                 // snapshot of our database.
-                float value = 40;//snapshot.getValue(float.class);
+                float value = snapshot.getValue(float.class);
                 if(value<=10){
                     String value1=Float.toString(value);
                     // after getting the value we are setting
